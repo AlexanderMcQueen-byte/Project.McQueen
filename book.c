98179c2c9bed8e84c6de44f90f7fb9a6e588b2de
AlexanderMcQueen-byte <alexandermcqueen136@gmail.com>
2024-10-25 08:26:57 +0300

    book.c

--- book.c
+++ book.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-struct book {
+struct Book {
     char title[30];
     char author[30];
     int publication_year;
@@ -10,21 +10,46 @@ struct book {
 };
 
 int main() {
-    struct book myBook;
+    struct Book book1;
 
-    // Part (ii): Initialize the structure using string literals and strcpy
-    strcpy(myBook.title, "Introduction to C Programming");
-    strcpy(myBook.author, "John Smith");
-    myBook.publication_year = 2022;
-    strcpy(myBook.ISBN, "9780131103627");
-    myBook.price = 49.99;
+    // Initialize book1 with the given data
+    strcpy(book1.title, "Introduction to C Programming");
+    strcpy(book1.author, "John Smith");
+    book1.publication_year = 2022;
+    strcpy(book1.ISBN, "9780131103627");
+    book1.price = 49.99;
 
-    // Part (iii): Print the values
-    printf("Title: %s\n", myBook.title);
-    printf("Author: %s\n", myBook.author);
-    printf("Publication Year: %d\n", myBook.publication_year);
-    printf("ISBN: %s\n", myBook.ISBN);
-    printf("Price: %.2f\n", myBook.price);
+    // Print the values of the fields
+    printf("Title: %s\n", book1.title);
+    printf("Author: %s\n", book1.author);
+    printf("Publication Year: %d\n", book1.publication_year);
+    printf("ISBN: %s\n", book1.ISBN);
+    printf("Price: %.2f\n", book1.price);
+
+    // Prompt the user to enter the declared variables and print them
+    printf("\nPlease enter the book details:\n");
+
+    printf("Title: ");
+    scanf("%s", book1.title);
+
+    printf("Author: ");
+    scanf("%s", book1.author);
+
+    printf("Publication Year: ");
+    scanf("%d", &book1.publication_year);
+
+    printf("ISBN: ");
+    scanf("%s", book1.ISBN);
+
+    printf("Price: ");
+    scanf("%f", &book1.price);
+
+    printf("\nBook Details:\n");
+    printf("Title: %s\n", book1.title);
+    printf("Author: %s\n", book1.author);
+    printf("Publication Year: %d\n", book1.publication_year);
+    printf("ISBN: %s\n", book1.ISBN);
+    printf("Price: %.2f\n", book1.price);
 
     return 0;
 }
