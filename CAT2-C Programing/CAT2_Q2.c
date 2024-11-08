5f5446fb6b868fb3a0d3c31a08f970b7048f813e
AlexanderMcQueen-byte <alexandermcqueen136@gmail.com>
2024-11-08 15:11:27 +0300

    Update CAT2_Q2.c

--- CAT2-C Programing/CAT2_Q2.c
+++ CAT2-C Programing/CAT2_Q2.c
@@ -2,14 +2,15 @@
 
 int main() {
     // Define and initialize the 2D array
+    int i,j;
     int scores[2][4] = {
         {65, 92,  84, 72},
         {35, 70,  59, 67}
     };
 
     // Print the elements using a nested for loop
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 4; j++) {
+    for ( i = 0; i < 2; i++) {
+        for ( j = 0; j < 4; j++) {
             printf(" %d ", scores[i][j]);
         }
         printf("\n");
